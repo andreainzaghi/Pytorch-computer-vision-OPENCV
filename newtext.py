b03db7c0b9a1a4acd8b329948707801a581076bd
andreainzaghi <andreainzaghilavoro@gmail.com>
2022-11-15 18:00:46 +0100

    opencv python

---
+++ newtext.py
@@ -0,0 +1,75 @@
+
+# apri la tab con l'immagine e al press di esc si chiude
+
+# import cv2
+# img = cv2.imread('./duck.png')
+# while True:
+#     cv2.imshow('duck', img)
+#     # if we have waited at least 1 ms AND we have pressed the escape
+#     if cv2.waitKey(1) & 0xFF == 27:
+#         break
+
+# cv2.destroyAllWindows()
+
+
+import cv2
+
+# function to display the coordinates of
+# of the points clicked on the image
+
+
+def click_event(event, x, y, flags, params):
+
+    # checking for left mouse clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(x) + ',' +
+                    str(y), (x, y), font,
+                    1, (255, 0, 0), 2)
+        cv2.imshow('image', img)
+
+    # checking for right mouse clicks
+    if event == cv2.EVENT_RBUTTONDOWN:
+
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv2.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r),
+                    (x, y), font, 1,
+                    (255, 255, 0), 2)
+        cv2.imshow('image', img)
+
+
+# driver function
+if __name__ == "__main__":
+
+    # reading the image
+    img = cv2.imread('./duck.png', 1)
+
+    # displaying the image
+    cv2.imshow('image', img)
+
+    # setting mouse handler for the image
+    # and calling the click_event() function
+    cv2.setMouseCallback('image', click_event)
+
+    # wait for a key to be pressed to exit
+    cv2.waitKey(0)
+
+    # close the window
+    cv2.destroyAllWindows()
